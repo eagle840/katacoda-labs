65b5666ebe4ec63cdaafb8d9583fe74f4bb91c98
eagle840 <eagle840@gmail.com>
2020-02-18 23:26:44 +0000

    swagger

--- swaggerandpy/assets/script.sql
+++ swaggerandpy/assets/script.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEST, phone_number TEST);
+CREATE TABLE users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, phone_number TEXT);
 INSERT INTO users (name, phone_number) VALUES ('john', '+1 85 123 1111');
 INSERT INTO users (name, phone_number) VALUES ('magre', '+1 814 123 1112');
 INSERT INTO users (name, phone_number) VALUES ('maggis', '+1 857 123 1114');
---
+++ swaggerandpy/resouces.md
@@ -0,0 +1,11 @@
+# Resources Uses & ideas
+
+
+## uses swagger petstore example
+
+https://github.com/hjacobs/connexion-example/blob/master/app.py
+
+## zalando hello world
+
+https://github.com/zalando/connexion/tree/master/examples/swagger2/helloworld
+
