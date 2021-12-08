8d0dcee53318b8bf500ba75915ccfa7e699dd77e
ir4engineer <39741327+eagle840@users.noreply.github.com>
2021-12-08 11:46:11 +0000

    rabbitmq

--- rabbitmq/assets/worker.py
+++ rabbitmq/assets/worker.py
@@ -1,7 +1,23 @@
-import sys
+#!/usr/bin/env python
+import pika
+import time
 
-message = ' '.join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body=message)
-print(" [x] Sent %r" % message)
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body.decode())
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+channel.start_consuming()
